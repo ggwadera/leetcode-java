bcc502b64760b35a069f318ef7d6005b85201021
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-20 21:46:30 +0100

    Solution for Flood Fill

---
+++ src/main/java/com/ggwadera/easy/FloodFill.java
@@ -0,0 +1,32 @@
+package com.ggwadera.easy;
+
+/**
+ * Solution for <a href="https://leetcode.com/problems/flood-fill">733. Flood Fill</a>
+ */
+public class FloodFill {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] != newColor) {
+            fill(image, sr, sc, image[sr][sc], newColor);
+        }
+        return image;
+    }
+
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0
+            || sr >= image.length
+            || sc < 0
+            || sc >= image[0].length) {
+            return;
+        }
+
+        if (color != image[sr][sc]) return;
+        image[sr][sc] = newColor;
+
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+    }
+
+}
