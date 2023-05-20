18a3dcccc2ffca408398426d4ed1d51d365cc0a0
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-20 18:51:11 +0100

    Solution for Merge Sorted Array

--- src/main/java/com/ggwadera/easy/MergeSortedArray.java
+++ src/main/java/com/ggwadera/easy/MergeSortedArray.java
@@ -6,10 +6,11 @@ package com.ggwadera.easy;
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the merged array
 
+        // Fill the merged array from the end
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i--];
