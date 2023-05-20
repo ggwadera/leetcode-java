c3cf9d899691a87b40ccb6ae46040f3b5baa983a
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-20 19:50:26 +0100

    Solution for Invert Binary Tree

---
+++ src/main/java/com/ggwadera/easy/InvertBinaryTree.java
@@ -0,0 +1,53 @@
+package com.ggwadera.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Solution for <a href="https://leetcode.com/problems/invert-binary-tree">226. Invert Binary Tree</a>
+ */
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+
+    public TreeNode invertTreeIterative(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode left = current.left;
+            TreeNode right = current.right;
+            current.right = left;
+            current.left = right;
+            if (left != null) queue.add(left);
+            if (right != null) queue.add(right);
+        }
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
