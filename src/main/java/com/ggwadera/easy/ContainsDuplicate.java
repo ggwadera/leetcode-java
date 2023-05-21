6ce083d24c806b4b8b79f9ba7af9f802aa40aa64
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-22 00:09:38 +0100

    Solution for Contains Duplicate

---
+++ src/main/java/com/ggwadera/easy/ContainsDuplicate.java
@@ -0,0 +1,36 @@
+package com.ggwadera.easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Solution for <a href="https://leetcode.com/problems/contains-duplicate">217. Contains Duplicate</a>
+ */
+public class ContainsDuplicate {
+
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Time: O(n log n)
+     * Space: O(1)
+     */
+    public boolean containsDuplicateSorted(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+
+}
