0c0b28dc2e320db7df98874a6044ba439fd24f97
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-21 22:57:42 +0100

    Solution for Longest Palindrome

---
+++ src/test/java/com/ggwadera/easy/LongestPalindromeTest.java
@@ -0,0 +1,30 @@
+package com.ggwadera.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class LongestPalindromeTest {
+
+    private static final LongestPalindrome lp = new LongestPalindrome();
+
+    @ParameterizedTest
+    @MethodSource("parameterSource")
+    void longestPalindrome(String s, int expected) {
+        assertThat(lp.longestPalindrome(s)).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> parameterSource() {
+        return Stream.of(
+            arguments("abccccdd", 7),
+            arguments("a", 1),
+            arguments("abcbe", 3),
+            arguments("ccc", 3)
+        );
+    }
+}
