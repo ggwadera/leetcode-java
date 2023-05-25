af3660080951f36f2e0cf290845affd90da847c1
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-25 15:03:11 +0100

    Solution for Remove Element

---
+++ src/main/java/com/ggwadera/easy/RemoveElement.java
@@ -0,0 +1,34 @@
+package com.ggwadera.easy;
+
+/**
+ * Solution for <a href="https://leetcode.com/problems/remove-element">27. Remove Element</a>
+ */
+public class RemoveElement {
+
+    /**
+     * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+     * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+     * <p>
+     * Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+     * <p>
+     * - Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+     * The remaining elements of nums are not important as well as the size of nums.
+     * <p>
+     * - Return k.
+     */
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            // When nums[i] is the value to be removed, swap with the element at the end of the array,
+            // also reducing the size of the array by one
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+}
---
+++ src/test/java/com/ggwadera/easy/RemoveElementTest.java
@@ -0,0 +1,59 @@
+package com.ggwadera.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class RemoveElementTest {
+
+    @ParameterizedTest
+    @MethodSource("parameterSource")
+    void removeElement(int[] nums, int val, int expected, int[] expectedNums) {
+        var r = new RemoveElement();
+        int k = r.removeElement(nums, val);
+        assertThat(k).isEqualTo(expected);
+        int[] resultNums = Arrays.copyOfRange(nums, 0, k);
+        assertThat(resultNums).containsExactlyInAnyOrder(expectedNums);
+    }
+
+    public static Stream<Arguments> parameterSource() {
+        return Stream.of(
+            arguments(
+                new int[]{3, 2, 2, 3},
+                3,
+                2,
+                new int[]{2, 2}
+            ),
+            arguments(
+                new int[]{0, 1, 2, 2, 3, 0, 4, 2},
+                2,
+                5,
+                new int[]{0, 1, 4, 0, 3}
+            ),
+            arguments(
+                new int[]{1},
+                3,
+                1,
+                new int[]{1}
+            ),
+            arguments(
+                new int[]{},
+                3,
+                0,
+                new int[]{}
+            ),
+            arguments(
+                new int[]{3},
+                3,
+                0,
+                new int[]{}
+            )
+        );
+    }
+}
