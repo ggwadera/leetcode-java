b5198fdfcb17d3b8e81544887158fa6e19c6bec8
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-27 18:53:35 +0100

    Solution for Validate Binary Tree Max Depth

---
+++ src/test/java/com/ggwadera/easy/BinaryTreeMaxDepthTest.java
@@ -0,0 +1,29 @@
+package com.ggwadera.easy;
+
+import com.ggwadera.common.TreeNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.ggwadera.utils.TreeUtils.parseTree;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class BinaryTreeMaxDepthTest {
+
+    @ParameterizedTest
+    @MethodSource("parameterSource")
+    void maxDepth(TreeNode root, int expected) {
+        var b = new BinaryTreeMaxDepth();
+        assertThat(b.maxDepth(root)).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> parameterSource() {
+        return Stream.of(
+            arguments(parseTree(3, 9, 20, null, null, 15, 7), 3),
+            arguments(parseTree(1, null, 2), 2)
+        );
+    }
+}
