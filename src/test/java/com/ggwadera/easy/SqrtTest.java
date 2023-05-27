4666d49c0ca9b9033a02cf76106406257fbba71a
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-05-27 22:08:56 +0100

    Solution for Sqrt(x)

---
+++ src/test/java/com/ggwadera/easy/SqrtTest.java
@@ -0,0 +1,27 @@
+package com.ggwadera.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SqrtTest {
+
+    @ParameterizedTest
+    @MethodSource("parameterSource")
+    void mySqrt(int x, int expected) {
+        var sqrt = new Sqrt();
+        assertThat(sqrt.mySqrt(x)).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> parameterSource() {
+        return Stream.of(
+            arguments(4, 2),
+            arguments(8, 2)
+        );
+    }
+}
