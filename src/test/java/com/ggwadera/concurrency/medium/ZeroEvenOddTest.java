a08732f943d65040c0ef36d4e008ffdd76aabc91
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2024-08-30 19:50:11 +0100

    Fix concurrency test

--- src/test/java/com/ggwadera/concurrency/medium/ZeroEvenOddTest.java
+++ src/test/java/com/ggwadera/concurrency/medium/ZeroEvenOddTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collectors;
@@ -22,14 +22,15 @@ class ZeroEvenOddTest {
         15, 010203040506070809010011012013014015
         """)
     void test(final int n, final String expected) throws InterruptedException {
-        final CountDownLatch latch = new CountDownLatch(2 * n);
-        final Queue<Integer> result = new LinkedBlockingQueue<>();
+        final CountDownLatch latch = new CountDownLatch(n);
+        final Collection<Integer> result = new LinkedBlockingQueue<>();
         ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(n);
         List<Thread> threads = getThreads(zeroEvenOdd, latch, result);
         Collections.shuffle(threads);
 
         threads.forEach(Thread::start);
         latch.await();
+        assertThat(result).hasSize(n * 2);
 
         String string = result.stream()
             .map(Object::toString)
@@ -37,14 +38,13 @@ class ZeroEvenOddTest {
         assertThat(string).isEqualTo(expected);
     }
 
-    private List<Thread> getThreads(ZeroEvenOdd zeroEvenOdd, CountDownLatch latch, Queue<Integer> result) {
+    private List<Thread> getThreads(ZeroEvenOdd zeroEvenOdd, CountDownLatch latch, Collection<Integer> result) {
         List<Thread> threads = new ArrayList<>();
         threads.add(
             new Thread(() -> {
                 try {
                     zeroEvenOdd.zero(i -> {
                         assertThat(i).as("Must be 0").isEqualTo(0);
-                        latch.countDown();
                         result.add(i);
                     });
                 } catch (InterruptedException e) {
@@ -57,8 +57,8 @@ class ZeroEvenOddTest {
                 try {
                     zeroEvenOdd.odd(i -> {
                         assertThat(i % 2).as("Must be odd number").isEqualTo(1);
-                        latch.countDown();
                         result.add(i);
+                        latch.countDown();
                     });
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -70,8 +70,8 @@ class ZeroEvenOddTest {
                 try {
                     zeroEvenOdd.even(i -> {
                         assertThat(i % 2).as("Must be even number").isEqualTo(0);
-                        latch.countDown();
                         result.add(i);
+                        latch.countDown();
                     });
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
